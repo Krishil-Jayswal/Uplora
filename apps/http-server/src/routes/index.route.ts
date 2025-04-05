c45f3e1924778ff98dd09ef87dbc423fda9d5e22
Krishil-Jayswal <jayswalkrishil@gmail.com>
2025-04-06 05:17:54 +0530

    User dashboard routes done.

--- apps/http-server/src/routes/index.route.ts
+++ apps/http-server/src/routes/index.route.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import authRouter from "./auth.route.js";
+import userRouter from "./user.route.js";
 
 const V1Router = express.Router();
 
 V1Router.use("/auth", authRouter);
+
+V1Router.use("/user", userRouter);
 
 export default V1Router;
