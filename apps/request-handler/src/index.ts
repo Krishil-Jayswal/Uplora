17a2a7ad707e7ab36ae930999e366ca44fc46f20
Krishil-Jayswal <jayswalkrishil@gmail.com>
2025-04-11 01:51:26 +0530

    Hostname and Filename from Request.

--- apps/request-handler/src/index.ts
+++ apps/request-handler/src/index.ts
@@ -2,7 +2,10 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
+app.get("/*", (c) => {
+  console.log(`Hostname: ${c.req.header("Host")}`);
+  console.log(`File requested: ${c.req.path}`);
+
   return c.text("Hello Hono!");
 });
 
