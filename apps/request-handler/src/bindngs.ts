49a8d7485984606cb94602f4dc505d6d286795ca
Krishil-Jayswal <jayswalkrishil@gmail.com>
2025-04-12 10:34:29 +0530

    Request Handler - Checkpoint 1

---
+++ apps/request-handler/src/bindngs.ts
@@ -0,0 +1,5 @@
+export interface Bindings {
+  BLOB_STORAGE_ACCOUNT: string;
+  BLOB_CONTAINER_NAME: string;
+  BLOB_SAS_TOKEN: string;
+}
--- apps/request-handler/src/index.ts
+++ apps/request-handler/src/index.ts
@@ -1,10 +1,24 @@
 import { Hono } from "hono";
+import { Bindings } from "./bindngs";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: Bindings;
+}>();
 
-app.get("/*", (c) => {
-  console.log(`Hostname: ${c.req.header("Host")}`);
-  console.log(`File requested: ${c.req.path}`);
+app.get("/*", async (c) => {
+  const hostname = c.req.header("Host");
+  const filename = c.req.path === "/" ? "index.html" : c.req.path.slice(1);
+  const slug = hostname?.split(".uplora")[0];
+
+  console.log(`Hostname: ${hostname}`);
+  console.log(`File requested: ${filename}`);
+  console.log(`Slug: ${slug}`);
+
+  const blobUrl = `https://${c.env.BLOB_STORAGE_ACCOUNT}.blob.core.windows.net/${c.env.BLOB_CONTAINER_NAME}/dist/${slug}/${filename}?${c.env.BLOB_SAS_TOKEN}`;
+
+  const response = await fetch(blobUrl);
+
+  console.log(response);
 
   return c.text("Hello Hono!");
 });
