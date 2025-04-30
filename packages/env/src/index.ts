dfeade14c41c4aac63950eee18aebc91e0f52f30
Krishil-Jayswal <jayswalkrishil@gmail.com>
2025-05-01 05:29:22 +0530

    Environment Variables pass on fixed and env-examples added.

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,26 +1,26 @@
 import { z } from "zod";
-import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import path from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const envPath = path.resolve(__dirname, "../../../.env");
-
-dotenv.config({ path: envPath });
+if (process.env.NODE_ENV === "development") {
+  const dotenv = await import("dotenv");
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const envPath = path.resolve(__dirname, "../../../.env");
+  dotenv.config({ path: envPath });
+}
 
 const EnvSchema = z.object({
-  HTTP_PORT: z.string(),
-  UPLOAD_PORT: z.string(),
-  REQ_HANDLER_PORT: z.string(),
-  JWT_SECRET: z.string(),
-  MAX_AGE: z.string(),
-  REDIS_URL: z.string(),
-  BLOB_CONNECTION_URL: z.string(),
-  BLOB_CONTAINER_NAME: z.string(),
-  PROXY_TARGET_URL: z.string(),
-  BLOB_SAS_TOKEN: z.string(),
+  HTTP_PORT: z.string().default("Your HTTP Server Port"),
+  UPLOAD_PORT: z.string().default("Your Upload Server Port"),
+  REQ_HANDLER_PORT: z.string().default("Your Request Handler Port"),
+  JWT_SECRET: z.string().default("Your JWT Secret"),
+  MAX_AGE: z.string().default("Your Token Max Age"),
+  REDIS_URL: z.string().default("Your Redis URL"),
+  BLOB_CONNECTION_URL: z.string().default("Your Azure Blob Connection URL"),
+  BLOB_CONTAINER_NAME: z.string().default("Your Azure Blob Container Name"),
+  PROXY_TARGET_URL: z.string().default("Your Proxy Target URL"),
+  BLOB_SAS_TOKEN: z.string().default("Your Azure Blob SAS Token"),
 });
 
 export const env = EnvSchema.parse(process.env);
