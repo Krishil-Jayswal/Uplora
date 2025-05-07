42e0cfde62952f219f23d64ecdd10bd931acabd5
Krishil-Jayswal <jayswalkrishil@gmail.com>
2025-05-07 17:11:19 +0530

    Added CD workflow for HTTP Server.

---
+++ scripts/ignore-build-check.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+APP_NAME=$1
+
+if [ -z "APP_NAME" ]; then
+    echo "No app name provided"
+    exit 1
+fi
+
+echo "Checking if '$APP_NAME' can be ignored..."
+
+set +e
+npx turbo-ignore "$APP_NAME"
+EXIT_CODE=$?
+set -e
+
+echo "Ignore check exited with exit code $EXIT_CODE"
+
+echo "status_code=$EXIT_CODE" >> "$GITHUB_OUTPUT"
